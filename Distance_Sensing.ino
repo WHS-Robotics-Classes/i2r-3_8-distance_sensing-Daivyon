b6a8e93741d8194f222a9daed6d4193dced5eb21
Daivyon <90796391+Daivyon@users.noreply.github.com>
2021-11-10 13:35:05 -0700

    Create Distance_Sensing.ino

---
+++ Distance_Sensing.ino
@@ -0,0 +1,58 @@
+/*
+    Ultrasonic sensor Pins:
+        VCC: +5VDC
+        Trig : Trigger (INPUT) - Pin11
+        Echo: Echo (OUTPUT) - Pin 12
+        GND: GND
+ */
+ 
+int trigPin = 11;    // Trigger
+int echoPin = 12;    // Echo
+long duration, cm, inches;
+ 
+void setup() {
+  //Serial Port begin
+  Serial.begin (9600);
+  //Define inputs and outputs
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(7, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(5, OUTPUT);
+}
+ 
+void loop() {
+  // The sensor is triggered by a HIGH pulse of 10 or more microseconds.
+  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(5);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+ 
+  // Read the signal from the sensor: a HIGH pulse whose
+  // duration is the time (in microseconds) from the sending
+  // of the ping to the reception of its echo off of an object.
+  pinMode(echoPin, INPUT);
+  duration = pulseIn(echoPin, HIGH);
+ 
+  // Convert the time into a distance
+  cm = (duration/2) / 29.1;     // Divide by 29.1 or multiply by 0.0343
+
+
+  Serial.print(cm);
+  Serial.print("cm");
+  Serial.println();
+  if(cm>30)
+  digitalWrite(7,HIGH);
+  digitalWrite(6,LOW);
+  digitalWrite(5,LOW);
+  if(cm>10 && cm<30)
+  digitalWrite(6,HIGH);
+  digitalWrite(7,LOW);
+  digitalWrite(5,LOW);
+  if(cm<10)
+  digitalWrite(5,HIGH);
+  digitalWrite(7,LOW);
+  digitalWrite(6,LOW);
+}
